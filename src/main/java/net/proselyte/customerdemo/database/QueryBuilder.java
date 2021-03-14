4596eef90430f287802db017a11125519fa75610
maximillian <maximilliaer@gmail.com>
2021-03-14 21:26:19 +0500

    старый класс

--- src/main/java/net/proselyte/customerdemo/database/QueryBuilder.java
+++ src/main/java/net/proselyte/customerdemo/database/QueryBuilder.java
@@ -7,19 +7,21 @@ import org.eclipse.swt.widgets.Text;
 import java.util.HashMap;
 import java.util.Map;
 
-public class QueryBielder {
+public class QueryBuilder { // старый класс для построение запроса в базу данных
     SWTTable table = new SWTTable();
+
     public static int conditionRows;
     public static int conditionRows1;
     public static Map<Integer, Combo> conditionsAttributes = new HashMap<Integer, Combo>(); //массив атрибутов поиска имя-фамилия и т.д.
     public static Map<Integer, Text> conditionsValues = new HashMap<Integer, Text>();      //массив значений ввода в строке поиска
     public static Map<Integer, Combo> conditionsOperators = new HashMap<Integer, Combo>();  //массив оператовров поиска и-или
-    public static Map<Integer, Combo> conditionsOperators1 = new HashMap<>();  //массив операторов поиска (IN, NOT IN) первой группы внутри скобок
+    public static Map<Integer, Combo> conditionsOperators1 = new HashMap<>();  //массив операторов поиска первой группы внутри скобок
 
     public static String GetCustomersQuery() {                                 //метод постороение запроса в базу данных в соответствии с значениями поиска
         String where = "";
         String lower = "LOWER";
         String like = "LIKE";
+        String searchOfDateOfBirthbefore = "";
         for (int i = 1; i <= conditionRows; i++) {
             Combo attributeField = conditionsAttributes.get(i);
             String attributeName = "";
@@ -32,6 +34,9 @@ public class QueryBielder {
             String attributeValue = valueField.getText();
 
             String attributeOperator = "";
+            String yearValue = "";
+            String monthValue = "";
+            String daysValue = "";
             if (i > 1) {
                 Combo operator = conditionsOperators.get(i);
                 Combo operator1 = conditionsOperators1.get(i);
@@ -46,8 +51,8 @@ public class QueryBielder {
                 continue;
             }
 
-            if (attributeName.equals("date_of_birth") || attributeName.equals("budget")) {
-                where = where + " " + attributeOperator + " (" + attributeName + " = '" + attributeValue + "')";                // вариант поиска по дате и бюджету
+            if (attributeName.equals("date_of_birth")) {
+                where = where + " " + attributeOperator + " (" + yearValue + "-" + monthValue + "-" + monthValue + "-" + daysValue + ");";                // вариант поиска по дате и бюджету
             } else {
                 where = where + " " + attributeOperator + " ( LOWER (" + attributeName + ") LIKE '" + attributeValue + "%')";   // вариант поиска по части текста
             }
