781b1c905ae3e7a6bd88e02928fdcfdc38bbdc8e
maximillian <maximilliaer@gmail.com>
2021-03-14 20:49:22 +0500

    FilterText

---
+++ src/main/java/net/proselyte/customerdemo/filters/FilterText.java
@@ -0,0 +1,38 @@
+package net.proselyte.customerdemo.filters;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import net.proselyte.customerdemo.model.Customer;
+
+import java.util.List;
+
+
+public class FilterText implements IFilter {
+
+
+    @Override
+    public String getAttribute() {
+        return null;
+    }
+
+    @Override
+    public String getValue() {
+        return null;
+    }
+
+    @Override
+    public String getOperator() {
+        return null;
+    }
+
+    @Override
+    public String getCondition() {
+        return null;
+    }
+
+    @Override
+    public String[] getOperatorsArray() {
+        return new String[0];
+    }
+}
