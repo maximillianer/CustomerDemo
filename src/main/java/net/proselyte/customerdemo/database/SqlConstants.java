eed2b91187d324b3a6e18ee1fe21a963d7519b57
maximillian <maximilliaer@gmail.com>
2021-03-16 23:21:20 +0500

    SQL constants

---
+++ src/main/java/net/proselyte/customerdemo/database/SqlConstants.java
@@ -0,0 +1,13 @@
+package net.proselyte.customerdemo.database;
+
+public class SqlConstants {
+    final static String AND = "AND";
+    final static String OR = "OR";
+    final static String BETWEEN = "BETWEEN";
+    final static String MORE = ">";
+    final static String LESS = "<";
+    final static String EQUAL = "=";
+    final static String LESS_OR_EQUAL = "<=";
+    final static String MORE_OR_EQUAL = ">=";
+
+}
