f0a17d9c3805eb4d37464d1cbe42ea171728f6ad
maximillian <maximilliaer@gmail.com>
2021-03-20 16:25:08 +0500

    инкапсуляция операторов и значений атрибутов поиска в окно SWT

--- src/main/java/net/proselyte/customerdemo/database/DBArrays.java
+++ src/main/java/net/proselyte/customerdemo/database/DBArrays.java
@@ -4,14 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DBArrays {
-    public static Map<String, String> sqlCommands = new HashMap<>();// команды поиска
-
-    public static String[] SQLOperators = new String[] {
-            "AND", "OR",
+    //   public static Map<String, String> sqlCommands = new HashMap<>();// команды поиска
+    //
+    public static String[] SQLOperators = new String[]{
+            "AND", "OR"
     };
 
     public static String[] SQLMathematicOperators = new String[]{
-            ">", "<", ">=", "<=", "=", "!=", "BETWEEN"
+            ">", "<", ">=", "<=", "=", "!=", "LIKE", "BETWEEN"
+    };
+
+    public static String[] SQLValues = new String[]{
+            "first_name", "last_name", "date_of_birth", "budget"
     };
 
 }
