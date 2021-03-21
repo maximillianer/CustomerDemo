4699fd63ca4fe74a9e6947877bfe00a983a46c11
maximillian <maximilliaer@gmail.com>
2021-03-21 17:45:10 +0500

    javaDoc

--- src/main/java/net/proselyte/customerdemo/database/DBManager.java
+++ src/main/java/net/proselyte/customerdemo/database/DBManager.java
@@ -6,15 +6,27 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 
-
+/**
+ *This class connected to PostgreSQL
+ * @author Maxim Eliseev
+ * @version 1.0
+ */
 public class DBManager {
 
     DBConnection db;
 
+    /**Method method connecting to postgresql*/
     public DBManager() { // установка подключения к БД
         this.db = new DBConnection("jdbc:postgresql://localhost:5432/customers", "org.postgresql.Driver", "postgres", "6503");
     }
 
+    /**
+     * the main method for processing a request SQL and searching for specified parameters
+     * @param query
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public ArrayList<Customer> GetAllParam(String query) throws ClassNotFoundException, SQLException { // основной метод поиска
         ArrayList<Customer> allParam = new ArrayList<>();
 
