47cb02c2135484bfc770a5a2ea9cb7775c0485e9
maximillian <maximilliaer@gmail.com>
2021-03-21 18:23:32 +0500

    javaDoc

--- src/main/java/net/proselyte/customerdemo/servise/CustomerService.java
+++ src/main/java/net/proselyte/customerdemo/servise/CustomerService.java
@@ -4,13 +4,23 @@ import net.proselyte.customerdemo.model.Customer;
 
 import java.util.List;
 
+/**
+ * This interfase for methods working with Customers
+ * @see CustomerServiceImpl
+ * @author Maxim Eliseev
+ * @version 1.0
+ */
 public interface CustomerService {
 
+    /**@see CustomerServiceImpl*/
     Customer getById(Long id); //найти по ID
 
+    /**@see CustomerServiceImpl*/
     void save (Customer customer); //сохранить customer
 
+    /**@see CustomerServiceImpl*/
     void delete(Long id); // удалить
 
+    /**@see CustomerServiceImpl*/
     List<Customer> getAll(); //получить всех
 }
